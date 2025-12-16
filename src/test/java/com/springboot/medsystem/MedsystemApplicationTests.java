e8f2110f09096afd317de23774a2c6f93c8aa71c
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-12-16 12:36:54 +0200

    initial commit

---
+++ src/test/java/com/springboot/medsystem/MedsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.medsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
